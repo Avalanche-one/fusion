6705e2851c0d9d1726da1418564347dcec62b43a
Avalanche-one <65412346+Avalanche-one@users.noreply.github.com>
2020-08-11 15:24:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+   
+    $('.projects__slider').slick({
+        dots: true,
+        infinite: false,
+        prevArrow: false,
+        nextArrow: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+    });
+
+    $('.testimonials__slider').slick({
+        dots: true,
+        infinite: false,
+        prevArrow: false,
+        nextArrow: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+   
+
+  
+});
